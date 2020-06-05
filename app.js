4a36e0c5c55059169904291726631d8ae420447a
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2020-06-05 15:05:05 +0530

    initial Commit

--- app.js
+++ app.js
@@ -1,68 +1,113 @@
 var twit = require('twit');
 var request = require('request');
+const express =  require("express")
+const app = express()
 
 var T = new twit({
-    consumer_key:         '/*Your Consumer Key*/',//consumer_key
-    consumer_secret:      '/*Your Consumer Secret*/',//consumer_secret
-    access_token:         '/*Your Access Token*/',//access_token
-    access_token_secret:  '/*Your Access Token Secret*/',//access_token secret
-});
+  consumer_key: process.env.consumer_key,//consumer_key
+  consumer_secret: process.env.consumer_secret,//consumer_secret
+  access_token: process.env.access_token,//access_token
+  access_token_secret: process.env.access_token_secret,//access_token secret
+})
 
 // Function for capitalizing the first word of the string
 const capitalize = (str, lower = false) =>
   (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
 ;
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+
+
+app.get("/" , (req,res)=>{
+  res.send("Follow <a href='https://twitter.com/piyushsthr'>@PiyushSthr</a> on twitter")
+})
+
+app.get("/MJuUhWgSk4gnk5bRm2X9DRXfYY7Mrc/hcqZmGebucRGxfRTHSGCRyWFgf6YGp/9anMqFhCQsbef7k4qmdv4m6Meue6zL",(req,res)=>{
+  main()
+})
+
+
+
+
+ var main = async()=>{
+
+  //Variables
+  let cases;
+  let deaths;
+  let recovered;
+
+  let casesIndia;
+  let deathsIndia;
+  let casesTodayIndia;
+  let todayRecoveredIndia;
+  let recoveredIndia;
+  let todayDeathsIndia;
+  let recoveredToday;
+  let deathsToday;
+
+  var text;
+
   //Getting Quotes from api
-request("https://disease.sh/v2/all",{json : true}, async(err,res,body)=>{
-    console.log(body.cases);
-    console.log(body.deaths);
-    
-    //Text you want to show in the image
-    let cases = numberWithCommas(body.cases);
-    let deaths = body.deaths;
-let recovered = body.recovered;
-		let active = body.active;
+   request("https://disease.sh/v2/all", { json: true }, (err, res, body) => {
+     console.log(body.cases);
+     console.log(body.deaths);
+     //Text you want to show in the image
+     cases = numberWithCommas(body.cases);
+     deaths = numberWithCommas(body.deaths);
+     recovered = numberWithCommas(body.recovered);
+     casesToday = numberWithCommas(body.todayCases)
+     recoveredToday = numberWithCommas(body.todayRecovered);
+     deathsToday = numberWithCommas(body.todayDeaths)
 
-var text = `
-Latest Covid-19 Cases! 👨‍🔬
+     let India = request("https://disease.sh/v2/countries/india", { json: true }, (err, res, body) => {
 
-WorldWide ☠
-Total Cases :- ${cases}
-Total Deaths :- ${deaths}
-Total Recovered :- ${recovered}
-Total Active :- ${active}
+      casesIndia = numberWithCommas(body.cases);
+      casesTodayIndia = numberWithCommas(body.todayCases);
+      deathsIndia = numberWithCommas(body.deaths);
+      todayRecoveredIndia = numberWithCommas(body.todayRecovered);
+      recoveredIndia = numberWithCommas(body.recovered);
+      todayDeathsIndia = numberWithCommas(body.todayDeaths);
+
+      text = `
+Latest Covid-19 News! 👨‍🔬
+
+WorldWide 🌍
+Cases Today :- ${casesToday} (${cases})
+Recovered Today :- ${recoveredToday} (${recovered})
+Deaths Today :- ${deathsToday} (${deaths})
 
 India 🇮🇳
-Total Cases :- 182,990
-Total Deaths :- 5,188
-Cases Today :- 1,163
+Cases Today :- ${casesTodayIndia} (${casesIndia})
+Recovered Today :- ${todayRecoveredIndia} (${recoveredIndia})
+Deaths Today :- ${todayDeathsIndia} (${deathsIndia})
 
 Stay Safe 😷
-`;
-console.log(text)
-
-		// tweet(text , quoteAuthor);
+      `;
+      console.log(text);
+      tweet(text);
 
     })
 
+   });
+
+}
 
 
 
 // uploading Image
-  function tweet(text){
-	
-	var tweet = text;
-    //Converting image to base64 to easily upload image on twitter servers
-    T.post('statuses/update', { status: tweet}, function(err, data, response) {
+function tweet(text) {
 
-  console.log(data);
+  var tweet = text;
+  //Converting image to base64 to easily upload image on twitter servers
+  T.post('statuses/update', { status: tweet }, function (err, data, response) {
 
-});
+    console.log(data);
+    console.log("Tweeted")
+
+  });
 
 
-  }
+}
 
